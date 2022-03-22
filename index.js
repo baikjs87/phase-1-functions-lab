ee9e9b3239a1bfff9016f87789837167a02319a5
Jason Baik <baikjs87@gmail.com>
2022-03-21 20:52:44 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    if (street >= 42) {
+        return street - 42
+    }
+    else {
+        return 42 - street
+    }
+}
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (start >= end) {
+        return (start - end) * 264
+    }
+    else {
+        return (end - start) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    const dist = distanceTravelledInFeet(start, destination)
+    if (dist < 400) {
+        return 0
+    }
+    else if (dist > 400 && dist < 2000) {
+        return (dist - 400) * 0.02
+    }
+    else if (dist > 2000 && dist < 2500) {
+        return 25
+    }
+    else {
+        return 'cannot travel that far'
+    }
+}
